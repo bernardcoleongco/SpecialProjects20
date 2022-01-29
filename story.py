38755a1c7be677fa244b22856843d4b6f9a2d6ae
bernardcoleongco <97357723+bernardcoleongco@users.noreply.github.com>
2022-01-29 10:45:43 -0700

    address comments

--- story.py
+++ story.py
@@ -1,11 +1,12 @@
 import csv
+import time
 
 prev_choices_dict = {}
 next_choices_dict = {}
 stories = {}
 
 def read_tsv():
-	# read tsv and store rows
+	# read tsv and print rows
 
 	tsv_file = open("story.tsv")
 	read_tsv = csv.reader(tsv_file, delimiter="\t")
@@ -15,35 +16,46 @@ def read_tsv():
 	  story = row[1]
 	  prompt = row[2]
 	  next_choices = row[3]
-
 	  for choice in prev_choices.split(','):
-	  	# clean up/standardize tsv data
-		if '0' not in choice:
+	  	if '0' not in choice:
 	  		choice = '0.' + choice
-	  	
-		prev_choices_dict[choice] = story
-	  
+	  	prev_choices_dict[choice] = story
 	  for choice in next_choices.split(','):
 	  	next_choices_dict[story] = choice
-
 	  stories[story] = prompt
-	
+
 	tsv_file.close()
 
 def read_user_input():
 	choices = '0'
 	prompt = None
+	the_end = 'The end.\n'
 	ending = "Press any key to exit."
-	while prompt != 'The end.\n' + ending:
+	error_message = "\nInvalid input, please try again.\n"
+
+	while prompt != the_end + ending:
 		story = prev_choices_dict[choices]
 		prompt = stories[story]+"\n"
 		print(story)
-		if prompt == 'The end.\n':
+		if prompt == the_end:
 			prompt += ending
 		if prompt == 'n/a\n':
 			choices = next_choices_dict[story]
 		else:
-			choices += "." + input(prompt)
+			user_selection = input(prompt)
+			if user_selection.isdigit():
+				prev_choice = choices
+				choices += "." + user_selection
+				if choices not in prev_choices_dict.keys():
+					print(error_message)
+					choices = prev_choice
+					time.sleep(1)
+			elif user_selection == 'q':
+				print("Thank you for playing!")
+				exit()
+			else:
+				print(error_message)
+				time.sleep(1)
 
 def main():
 	read_tsv()
