fde443db053222072c0046828b08a49ee2d5021b
bernardcoleongco <97357723+bernardcoleongco@users.noreply.github.com>
2022-01-29 10:11:55 -0700

    full user interactivity

--- story.py
+++ story.py
@@ -31,7 +31,19 @@ def read_tsv():
 	tsv_file.close()
 
 def read_user_input():
-	user_input = input()
+	choices = '0'
+	prompt = None
+	ending = "Press any key to exit."
+	while prompt != 'The end.\n' + ending:
+		story = prev_choices_dict[choices]
+		prompt = stories[story]+"\n"
+		print(story)
+		if prompt == 'The end.\n':
+			prompt += ending
+		if prompt == 'n/a\n':
+			choices = next_choices_dict[story]
+		else:
+			choices += "." + input(prompt)
 
 def main():
 	read_tsv()
