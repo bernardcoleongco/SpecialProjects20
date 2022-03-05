9614b2b6b4edb5a2d55568f107a67eb3c8ab3d4a
bernardcoleongco <97357723+bernardcoleongco@users.noreply.github.com>
2022-03-05 08:18:11 -0700

    address comments

--- calculator.py
+++ calculator.py
@@ -1,35 +1,77 @@
-import datetime
+from datetime import date
  
-all_data = {
-    "Transaction Type": "",
-    "Origin or Destination": "",
-    "Amount": "",
-    "Date": ""
+transaction_data = {
+    "transaction_type": "",
+    "amount": float(),
+    "date": ""
 }
  
-def data():
+def get_transaction_type():
+    valid_income_transaction = ["i", "income",  "inc",  "+"]
+    valid_expense_transaction = ["e", "expense","exp", "-"]
     # input either income or expense
-    choose = input("Income or Expense? ")
-    all_data.update({"Transaction Type": choose})
- 
-    # from/to, amount, date (optional)
-    choose2 = input("From or where to is this money going? ")
-    all_data.update({"Origin or Destination": choose2})
-    if choose == "Income" or choose == "Expense":
-        transactionAmount = input("How much money was involved in this transaction? ")
-        all_data.update({"Amount": transactionAmount})
+    transaction_type = input("Income or Expense? Input one of: " + " ".join(valid_income_transaction) + " or " + " ".join(valid_expense_transaction) + "\n")
+   
+    # validate
+    income = False
+    expense = False
+    if transaction_type in valid_income_transaction:
+        transaction_data.update({"transaction_type": "income"})
+        income = True
+    elif transaction_type in valid_expense_transaction:
+        transaction_data.update({"transaction_type": "expense"})
+        expense = True
     else:
-        print("You did not answer")
+        income, expense = get_transaction_type()
+    return income, expense
  
-    transactionDate = input("When did you make this transaction? (if not applicable, type n) ")
+def get_source_reason(income, expense):
+    if income:
+        source = input("Source of income? ")
+        if len(source):
+            transaction_data.update({"source": source})
+        else:
+            get_source_reason(income, expense)
  
-    if transactionDate == "n":
-        transactionDate = datetime.datetime.now()
-        all_data.update({"Date": transactionDate})
+    elif expense:
+        reason = input("Reason for expense? ")
+        if len(reason):
+            transaction_data.update({"reason": reason})
+        else:
+            get_source_reason(income, expense)
  
+ 
+def get_amount():
+    # make sure it's an integer or float
+    amount = input("Transaction amount in dollars: ")
+    try:
+        amount = float(amount)
+        transaction_data.update({"amount": amount})
+    except ValueError:
+        get_amount()
+ 
+def get_date():
+    today = date.today()
+    transaction_date = input("Date of transaction as dd/mm/yyyy? (Hit enter to default to today's date) ")
+    day, month, year = transaction_date.split("/")
+ 
+    # validate
+    try:
+        year = int(year)
+        month = int(month)
+        day = int(day)
+        transaction_date = date(year, month, day)
+    except ValueError:
+        get_date()
+   
+    if transaction_date <= today:
+        transaction_data.update({"date": transaction_date.strftime("%d/%m/%Y")})
  
 def main():
-    data()
-    print(all_data)
+    income, expense = get_transaction_type()
+    get_source_reason(income, expense)
+    get_amount()
+    get_date()
+    print(transaction_data)
  
 main()
